e17192274a58d4c4fe0d5107e20a6870f810b460
Muchamad Fatchur Rachman <fatchurrachman001@gmail.com>
2026-04-23 14:11:45 +0700

    update jari jari

--- modul3_belajarPHP/tugas2.php
+++ modul3_belajarPHP/tugas2.php
@@ -1,10 +1,10 @@
 <?php
-    $jari_jari = 7;
+    $r = 7;
     $selimut = 10;
     $phi = 3.14;
-    $luas_alas = $phi * $jari_jari * $jari_jari;
-    $luas_permukaan = ($phi * $jari_jari * $jari_jari) + ($phi * $jari_jari * $selimut);
-    echo "Luas alas kerucut dengan jari-jari $jari_jari adalah : $luas_alas <br>";
-    echo "Luas permukaan kerucut dengan jari-jari $jari_jari dan selimut $selimut adalah : $luas_permukaan";
+    $luas_alas = $phi * $r * $r;
+    $luas_permukaan = ($phi * $r * $r) + ($phi * $r * $selimut);
+    echo "Luas alas kerucut dengan jari-jari $r adalah : $luas_alas cm<sup>2</sup><br>";
+    echo "Luas permukaan kerucut dengan jari-jari $r dan selimut $selimut adalah : $luas_permukaan cm<sup>2</sup>";
 ?>
 
