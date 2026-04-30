a3969e63ee44334076641b4bf4f0b6d09de5ad94
Muchamad Fatchur Rachman <fatchurrachman001@gmail.com>
2026-04-30 14:55:43 +0700

    crud

---
+++ modul4/config.php
@@ -0,0 +1,17 @@
+<?php
+
+$databaseHost = 'localhost';
+
+$databaseName = 'simrs';
+
+$databaseUsername = 'root';
+
+$databasePassword = '';
+
+
+
+// Make sure the variable name is $mysqli
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+?>
