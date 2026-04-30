3cdd762cd9f4c2ee558e7f6073e02405bde616bc
Muchamad Fatchur Rachman <fatchurrachman001@gmail.com>
2026-04-30 13:29:34 +0700

    crud php

---
+++ modul4_belajar/config.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+//host
+
+$databaseHost = 'localhost';
+
+//Nama database
+
+$databaseName = 'simrs';
+
+//Username
+
+$databseUsername = 'root';
+
+//Password mysql
+
+$databasePassword = '';
+
+
+
+$mysqli = mysqli_connect($databaseHost, $databseUsername, $databasePassword, $databaseName);
