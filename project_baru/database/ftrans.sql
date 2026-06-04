df9bbc69755117830777cab6aa0cce6acee9e72e
Muchamad Fatchur Rachman <fatchurrachman001@gmail.com>
2026-06-04 14:56:30 +0700

    ganti sql

--- project_baru/database/ftrans.sql
+++ project_baru/database/ftrans.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 11, 2026 at 05:31 AM
+-- Generation Time: Jun 04, 2026 at 08:43 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -31,16 +31,25 @@ CREATE TABLE `kendaraan` (
   `kode_unik_kendaraan` int(10) NOT NULL,
   `nama_kendaraan` varchar(50) DEFAULT NULL,
   `jenis_kendaraan` enum('roda 2','roda 4') DEFAULT NULL,
-  `harga_per_hari` int(10) DEFAULT NULL
+  `harga_per_hari` int(10) DEFAULT NULL,
+  `gambar` longblob DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `kendaraan`
+-- Table structure for table `penyewaan`
 --
 
-INSERT INTO `kendaraan` (`kode_unik_kendaraan`, `nama_kendaraan`, `jenis_kendaraan`, `harga_per_hari`) VALUES
-(990, 'MIO', 'roda 2', 50000),
-(51123, 'Supra 12512', 'roda 4', 100000);
+CREATE TABLE `penyewaan` (
+  `id_sewa` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `kode_unik_kendaraan` int(10) NOT NULL,
+  `tanggal_sewa` datetime NOT NULL,
+  `tanggal_kembali` datetime NOT NULL,
+  `total_biaya` int(11) DEFAULT NULL,
+  `status` enum('booking','sedang_disewa','selesai','dibatalkan') DEFAULT 'booking'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -61,7 +70,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `nama`, `email`, `password`, `created_at`) VALUES
-(1, 'fatchur', 'a@gmail.com', '$2y$10$O5o.ZBAR7th/sVcrmD944OlyDlVY20ev5p9jqa.qfcxao0qHbgvWC', '2026-05-08 02:37:05');
+(1, 'fatchur', 'a@gmail.com', '$2y$10$O5o.ZBAR7th/sVcrmD944OlyDlVY20ev5p9jqa.qfcxao0qHbgvWC', '2026-05-08 02:37:05'),
+(2, 'bagas', 'b@gmail.com', '$2y$10$NVpgcRk3ZdZX1Wbi4iHXR.Le.BW4F1GHr6Mhxzuf6Rxiyy/evGldC', '2026-05-13 06:33:31');
 
 --
 -- Indexes for dumped tables
@@ -72,6 +82,14 @@ INSERT INTO `users` (`id`, `nama`, `email`, `password`, `created_at`) VALUES
 --
 ALTER TABLE `kendaraan`
   ADD PRIMARY KEY (`kode_unik_kendaraan`);
+
+--
+-- Indexes for table `penyewaan`
+--
+ALTER TABLE `penyewaan`
+  ADD PRIMARY KEY (`id_sewa`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `kode_unik_kendaraan` (`kode_unik_kendaraan`);
 
 --
 -- Indexes for table `users`
@@ -85,10 +103,27 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `penyewaan`
+--
+ALTER TABLE `penyewaan`
+  MODIFY `id_sewa` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `penyewaan`
+--
+ALTER TABLE `penyewaan`
+  ADD CONSTRAINT `penyewaan_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `penyewaan_ibfk_2` FOREIGN KEY (`kode_unik_kendaraan`) REFERENCES `kendaraan` (`kode_unik_kendaraan`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
