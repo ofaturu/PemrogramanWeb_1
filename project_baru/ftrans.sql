633c9c53fa43f1ba6013fb55a3e4a01ceea1a5df
Fatchur <fatchurrachman001@gmail.com>
2026-06-19 09:56:06 +0700

    database

---
+++ project_baru/ftrans.sql
@@ -0,0 +1,174 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 19, 2026 at 04:54 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ftrans`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kendaraan`
+--
+
+CREATE TABLE `kendaraan` (
+  `kode_unik_kendaraan` int(10) NOT NULL,
+  `id_merk` int(50) NOT NULL,
+  `nama_kendaraan` varchar(50) DEFAULT NULL,
+  `jenis_kendaraan` enum('roda 2','roda 4') DEFAULT NULL,
+  `harga_per_hari` int(10) DEFAULT NULL,
+  `gambar` longblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `merk_kendaraan`
+--
+
+CREATE TABLE `merk_kendaraan` (
+  `id_merk` int(10) NOT NULL,
+  `nama_merk` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `merk_kendaraan`
+--
+
+INSERT INTO `merk_kendaraan` (`id_merk`, `nama_merk`) VALUES
+(1, 'honda'),
+(2, 'toyota'),
+(3, 'daihatsu'),
+(4, 'kia');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penyewaan`
+--
+
+CREATE TABLE `penyewaan` (
+  `id_sewa` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `kode_unik_kendaraan` int(10) NOT NULL,
+  `tanggal_sewa` datetime NOT NULL,
+  `tanggal_kembali` datetime NOT NULL,
+  `total_biaya` int(11) DEFAULT NULL,
+  `status` enum('booking','sedang_disewa','selesai','dibatalkan') DEFAULT 'booking'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `nama`, `email`, `password`, `created_at`) VALUES
+(1, 'fatchur', 'a@gmail.com', '$2y$10$T2irbai2z4cqvHqXZwL5qumnXUoZ6nIVLaZyr18rxPtjpvgjPS.UW', '2026-05-08 02:37:05'),
+(2, 'bagas', 'b@gmail.com', '$2y$10$NVpgcRk3ZdZX1Wbi4iHXR.Le.BW4F1GHr6Mhxzuf6Rxiyy/evGldC', '2026-05-13 06:33:31'),
+(3, 'Test Operator Updated', 'testop@ftrans.com', '$2y$10$CksAaIy3ywj6K/jz.6D8pu0ZUk0L5oCKmAHDX.8o3ax7uwCRFAYnu', '2026-06-04 07:40:37'),
+(4, 'Test User', 'test@example.com', '$2y$10$voyGYVCdrXnwncEfHK6v4uuW7EbgSgUhJzE18xenxkz/wsOdEAwXK', '2026-06-18 07:26:53');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kendaraan`
+--
+ALTER TABLE `kendaraan`
+  ADD PRIMARY KEY (`kode_unik_kendaraan`),
+  ADD KEY `id_merk` (`id_merk`);
+
+--
+-- Indexes for table `merk_kendaraan`
+--
+ALTER TABLE `merk_kendaraan`
+  ADD PRIMARY KEY (`id_merk`);
+
+--
+-- Indexes for table `penyewaan`
+--
+ALTER TABLE `penyewaan`
+  ADD PRIMARY KEY (`id_sewa`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `kode_unik_kendaraan` (`kode_unik_kendaraan`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `merk_kendaraan`
+--
+ALTER TABLE `merk_kendaraan`
+  MODIFY `id_merk` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `penyewaan`
+--
+ALTER TABLE `penyewaan`
+  MODIFY `id_sewa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `kendaraan`
+--
+ALTER TABLE `kendaraan`
+  ADD CONSTRAINT `kendaraan_ibfk_1` FOREIGN KEY (`id_merk`) REFERENCES `merk_kendaraan` (`id_merk`);
+
+--
+-- Constraints for table `penyewaan`
+--
+ALTER TABLE `penyewaan`
+  ADD CONSTRAINT `penyewaan_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `penyewaan_ibfk_2` FOREIGN KEY (`kode_unik_kendaraan`) REFERENCES `kendaraan` (`kode_unik_kendaraan`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
